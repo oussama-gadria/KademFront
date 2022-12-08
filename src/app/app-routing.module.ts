2305a479f50560e9e2e027cf85395dcd2cb6dbf1
Oussama Gadria <gadria.oussama@esprit.tn>
2022-12-08 19:18:16 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path:"equipe",component:HomeComponent}
+  {path:"equipe",component:HomeComponent},
+  {path:"universite",loadChildren:()=>import('./universite/universite.module').then(m=>m.UniversiteModule)},
+  {path:"enseignant",loadChildren:()=>import('./enseignant/enseignant.module').then(m=>m.EnseignantModule)}
 ];
 
 @NgModule({
