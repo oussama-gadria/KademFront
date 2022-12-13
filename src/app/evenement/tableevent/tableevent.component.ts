0eec274811f23f80b0e757fafedda5a3ab30a5eb
houssemkharroubi <houssem.kharroubiessayed@esprit.tn>
2022-12-13 02:48:13 +0100

    +input

--- src/app/evenement/tableevent/tableevent.component.ts
+++ src/app/evenement/tableevent/tableevent.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { evenement } from 'src/app/Core/models/evenement';
 
 @Component({
   selector: 'app-tableevent',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tableevent.component.css']
 })
 export class TableeventComponent implements OnInit {
-
+@Output() GoToUpdateEvenementevent =new EventEmitter();
+@Output() getIdToDeleteevent =new EventEmitter();
+@Input() Listevent:evenement[];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  GoToUpdateEvenement(id:number){
+    this.GoToUpdateEvenementevent.emit(id);
+  };
+  getIdToDelete(id:number){
+    this.getIdToDeleteevent.emit(id);
+  };
 }
